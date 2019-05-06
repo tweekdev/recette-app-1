75298bb9546fb002fa422f9e991f80b5ccbed5d2
tweek.dev <tja.theau54@gmail.com>
2019-05-06 20:58:29 +0200

    components cards recettes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react'
 // CSS
 import './App.css'
-
+import Admin from './components/Admin'
+import Card from './components/Card'
+import Header from './components/Header'
+import recettes from './recettes'
 class App extends Component {
   state = {
     pseudo: this.props.match.params.pseudo,
+    recettes: {},
   }
 
+  chargerExemple = () => this.setState({ recettes })
+
   render() {
+    const cards = Object.keys(this.state.recettes).map(key => (
+      <Card key={key} details={this.state.recettes[key]} />
+    ))
+
     return (
       <div className='box'>
+        <Header pseudo={this.state.pseudo} />
         <h1>Bonjour {this.state.pseudo}</h1>
-        <div className='cards'>
-          <div className='card'>
-            <h2>Une Carte</h2>
-          </div>
-        </div>
+        <div className='cards'>{cards}</div>
+        <Admin chargerExemple={this.chargerExemple} />
       </div>
     )
   }
